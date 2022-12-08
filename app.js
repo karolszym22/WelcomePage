b28835ab95526842cb0372fd7da0c7dc1d4e6ece
Karol Szymański <karol2134@op.pl>
2022-12-08 17:48:04 +0100

    Adding and editing -_wrapper.scss: adding new animate: animate-alternative from one to four and change border in scroll-nav-border

--- app.js
+++ app.js
@@ -1,52 +1,37 @@
-
-
-
-const colors = ['orange', 'blue']
-
-
-const headerContainer = document.querySelector('header')
-const authorName = document.querySelector('.author-name');
-const bottomMenu = document.querySelector('.bottom-menu');
-const contactNav = document.querySelector('.contact-nav');
-const phoneNumber = document.querySelector('.contact');
-const headerNav = document.querySelectorAll('.header-nav');
-const contactSpan = document.querySelectorAll('.contact-span')
-window.addEventListener('scroll', function () {
-
-  const scrollFromTop = window.pageYOffset
-  if(scrollFromTop > 50)
-  {
-    bottomMenu.classList.add('scroll-border')
-    headerContainer.classList.add('scroll-background');
-    authorName.classList.add('scroll-author-title');
-    phoneNumber.classList.add('scroll-nav')
-    contactNav.classList.add('scroll-nav-border')
-    headerNav.forEach((e)=>{
-      e.classList.add('scroll-nav');
-    })
-    contactSpan.forEach((e)=>{
-      e.classList.add('scroll-nav-contact-nav');
-    })
-  }else
-  {
-    bottomMenu.classList.remove('scroll-border')
-    headerContainer.classList.remove('scroll-background');
-    authorName.classList.remove('scroll-author-title');
-    phoneNumber.classList.remove('scroll-nav')
-    headerNav.forEach((e)=>{
-      e.classList.remove('scroll-author-title');
-    })
-    contactSpan.forEach((e)=>{
-      e.classList.remove('scroll-nav-contact-nav');
-    })
+const headerContainer = document.querySelector("header");
+const authorName = document.querySelector(".author-name");
+const bottomMenu = document.querySelector(".bottom-menu");
+const contactNav = document.querySelector(".contact-nav");
+const phoneNumber = document.querySelector(".contact");
+const headerNav = document.querySelectorAll(".header-nav");
+const contactSpan = document.querySelectorAll(".contact-span");
+window.addEventListener("scroll", function () {
+  const scrollFromTop = window.pageYOffset;
+  if (scrollFromTop > 50) {
+    bottomMenu.classList.add("scroll-border");
+    headerContainer.classList.add("scroll-background");
+    authorName.classList.add("scroll-author-title");
+    phoneNumber.classList.add("scroll-nav");
+    contactNav.classList.add("scroll-nav-border");
+    headerNav.forEach((e) => {
+      e.classList.add("scroll-nav");
+    });
+    contactSpan.forEach((e) => {
+      e.classList.add("scroll-nav-contact-nav");
+    });
+  } else {
+    bottomMenu.classList.remove("scroll-border");
+    headerContainer.classList.remove("scroll-background");
+    authorName.classList.remove("scroll-author-title");
+    phoneNumber.classList.remove("scroll-nav");
+    contactNav.classList.remove("scroll-nav-border");
+    headerNav.forEach((e) => {
+      e.classList.remove("scroll-author-title");
+    });
+    contactSpan.forEach((e, index) => {
+      e.classList.remove("scroll-nav-contact-nav");
+    });
   }
+});
 
-})
-
-
-
-
-
-
-var typed = new Typed('.element', options)
-
+var typed = new Typed(".element", options);
