fe344ea460b3e1ac4b0e978ef5d1074085e13b82
Karol Szymański <karol2134@op.pl>
2023-01-08 22:00:40 +0100

    Adding animation to antoher presentation-container's elements

--- js/Presentation.js
+++ js/Presentation.js
@@ -17,17 +17,24 @@ const StartPresentation = () =>{
       }
       setTimeout(function () {
         if (n === MAX_ANIMATIONS_NUMBER) {
+          presentationHeader.classList.add(animationArray[n - 1]);
+          presentationNav.classList.add(animationArray[n - 1]);
           presentationFooter.classList.add(animationArray[n - 1]);
+          presentationMain.classList.add(animationArray[n - 1])
           console.log(presentationFooter)
         } else {
+          presentationHeader.classList.remove(animationArray[n]);
+          presentationNav.classList.remove(animationArray[n]);
           presentationFooter.classList.remove(animationArray[n]);
-    
+          presentationMain.classList.remove(animationArray[n]);
+          presentationHeader.classList.add(animationArray[n - 1]);
           presentationFooter.classList.add(animationArray[n - 1]);
+          presentationMain.classList.add(animationArray[n - 1]);
           console.log(presentationFooter)
         }
     
         recursiveFunction(n - 1);
-      }, 2000);
+      }, 5000);
     }
     
     recursiveFunction(MAX_ANIMATIONS_NUMBER);
